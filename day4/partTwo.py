d9076cfd0ae9034ff3ef9322f34344447aef7d54
ozzy <businessozzy27@gmail.com>
2025-12-04 00:38:43 -0500

    day4

---
+++ day4/partTwo.py
@@ -0,0 +1,41 @@
+import os
+from pathlib import Path
+
+scriptDir = Path(__file__).resolve().parent
+inputPath = scriptDir / "input"
+
+with inputPath.open() as f:
+    rawData = f.read().strip()
+
+lines = rawData.splitlines()
+paperGrid = [list(line) for line in lines]
+adjacentOffsets = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
+numRows = len(paperGrid)
+numCols = len(paperGrid[0]) if numRows else 0
+
+totalRemoved = 0
+while True:
+    toRemove = []
+    for row in range(numRows):
+        for col in range(numCols):
+            if paperGrid[row][col] != '@':
+                continue                           
+            adjacentCount = 0
+            for dRow, dCol in adjacentOffsets:
+                nRow, nCol = row + dRow, col + dCol
+                if 0 <= nRow < numRows and 0 <= nCol < numCols and paperGrid[nRow][nCol] == '@':
+                    adjacentCount += 1
+            if adjacentCount < 4:
+                toRemove.append((row, col))
+    if not toRemove:
+        break
+    for (r, c) in toRemove:
+        paperGrid[r][c] = '.'
+    totalRemoved += len(toRemove)
+
+print(totalRemoved)
+
+#i am sorry if ts is terrible to read
+# i am cooked for my math test tmrw
+# it is 12:38 rn
+# good night
